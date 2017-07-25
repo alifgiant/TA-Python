8629c01303e99faece9aee47a5050b42cb4f0c49
alifgiant <muh.alifgiant@gmail.com>
2017-07-25 17:35:20 +0700

    reorganize

--- MIT_BIH/test.py
+++
@@ -1,8 +0,0 @@
-
-if __name__ == '__main__':
-    a_file = open('res.txt', 'w')
-    print('something', file=a_file)
-    print('something2', file=a_file)
-    print('something3', file=a_file)
-    a_file.close()
-
--- exec_time/show_exec.py
+++ exec_time/show_exec.py
@@ -1,18 +1,24 @@
 import json
 
+# base = 'sensor/'
+base = 'server/'
+
 def avg(arr):
 	return sum(arr)/len(arr)
 
 def output(list_arr):
-	# getMS = lambda x: (x/1000000)-5 if (x/1000000)-5 > 0 else 0
-	getMS = lambda x: x/1000
-	# return [getMS(x[1]) for x in list_arr[2:100]]
-	return [getMS(x) for x in list_arr[1:]]
+	# TIME IN SERVER	
+	getMS = lambda x: x/1000000  # time exec, ms
+	return [getMS(x[1]) for x in list_arr[3:100]]
+
+	# TIME IN SENSOR
+	# getMS = lambda x: x/1000
+	# return [getMS(x) for x in list_arr[1:]]
 
 def main():
-	delay1 = json.load(open('out1.json'))	
-	delay2 = json.load(open('out2.json'))
-	delay3 = json.load(open('out3.json'))
+	delay1 = json.load(open(base+'data1.json'))	
+	delay2 = json.load(open(base+'data2.json'))
+	delay3 = json.load(open(base+'data3.json'))
 
 	delay1 = output(delay1)	
 	delay2 = output(delay2)
